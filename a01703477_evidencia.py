1d70a0b36bf4b65ac430b49f80cf0e1bc08dfc25
SaidFlores22 <131408259+SaidFlores22@users.noreply.github.com>
2023-06-05 19:50:44 -0600

    Update a01703477_evidencia.py

--- a01703477_evidencia.py
+++ a01703477_evidencia.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 st.title('Police Incident Reports from 2018 and 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view")
+df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present (1).csv")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
 mapa=pd.DataFrame(longitude)
-mapa['Longitude'] = df['Longitude']
 mapa['Latitude'] = df['Latitude']
+mapa['Longitude'] = df['Longitude']
 mapa=mapa.dropna()
 st.map(mapa.astype(int))
