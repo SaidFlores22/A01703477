58b57a145933af0860d83cdae1a5872cbd700a3f
SaidFlores22 <131408259+SaidFlores22@users.noreply.github.com>
2023-06-05 22:38:58 -0600

    Update a01703477_evidencia.py

--- a01703477_evidencia.py
+++ a01703477_evidencia.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
 
-with open(file_name, "rb") as template_file:
-        template_byte = template_file.read()
-
-    st.download_button(label="Click to Download Template File",
-                        data=template_byte,
-                        file_name="Police_Department_Incident_Reports__2018_to_Present.xlsx",
-                        mime='application/octet-stream')
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+df_xlsx = to_excel(df)
+st.download_button(label='📥 Download Current Result',
+                                data=df,
+                                file_name= 'Police_Department_Incident_Reports__2018_to_Present.xlsx')
 
 st.title('Police Incident Reports from 2018 and 2020 in San Francisco')
 
