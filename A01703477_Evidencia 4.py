d4b5ba8bf2909f40ac5c1dc32988301e6b91ca14
SaidFlores22 <131408259+SaidFlores22@users.noreply.github.com>
2023-06-05 18:51:37 -0600

    Update A01703477_Evidencia.py

--- A01703477_Evidencia 4.py
+++ A01703477_Evidencia 4.py
@@ -1,14 +1,14 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 
 
-st.title('Police department incident reports from 2018 to 2020 in San Francisco')
+st.title ('Police Incident Reports from 2018 and 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=drive_link")
+df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa=pd.DataFrame() #Modificar latitud y longitud
+mapa=pd.DataFrame()
 mapa=mapa.dropna()
 st.map(mapa.astype(int))
 
